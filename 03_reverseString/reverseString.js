d33aa1ae30be71066cc15f29f10a16d0f181f79f
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-09 21:47:09 +0530

    Solve problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let length = string.length;
+  let reverseString = "";
+  for (let i = length - 1; i >= 0; i--) {
+    reverseString = reverseString.concat(string.charAt(i));
+  }
+  return reverseString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
