c85ba1415c38a56763994b1ce779af56d478ff99
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-09 21:23:10 +0530

    Solve problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
 
+  if (n < 0) {
+    return 'ERROR';
+  }
+  let answer = "";
+  for (let i = 0; i < n; i++) {
+    answer = answer + string;
+  }
+  return answer;
 };
 
 // Do not edit below this line
