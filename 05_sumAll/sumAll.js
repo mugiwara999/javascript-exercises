202ce003db474bf9e62ea62c6c6581081f6becc9
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-10 11:37:17 +0530

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startInteger, endInteger) {
 
+  if (startInteger > endInteger) {
+    let tmp = endInteger;
+    endInteger = startInteger;
+    startInteger = tmp;
+  }
+
+  if (typeof (startInteger) !== 'number' || typeof (endInteger) !== 'number') {
+    return 'ERROR';
+  }
+
+  if (startInteger < 0 || endInteger < 0) {
+    return "ERROR"
+  }
+  let finalSum = 0;
+
+  for (let i = startInteger; i <= endInteger; i++) {
+
+    finalSum = finalSum + i;
+  }
+
+  return finalSum;
 };
 
 // Do not edit below this line
