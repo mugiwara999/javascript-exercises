b38b44190062437b12789094d6f2de9ef5f4165a
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-10 11:24:16 +0530

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
 const removeFromArray = function() {
 
+  let length = arguments[0].length;
+
+  let argumentLength = arguments.length;
+
+  for (let j = 0; j < argumentLength; j++) {
+
+
+    for (let i = 0; i < length; i++) {
+
+      if (arguments[j] === arguments[0][i]) {
+        arguments[0].splice(i, 1);
+        ;
+      }
+    }
+  }
+  return arguments[0];
+
 };
 
 // Do not edit below this line
