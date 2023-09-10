602458c5b2bb84cd5e09d1c1eee64a63095f2b1a
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-10 14:17:01 +0530

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,57 @@
-const add = function() {
-	
-};
+const add = function(number1, number2) {
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+  return number1 + number2;
 
 };
 
-const power = function() {
-	
+const subtract = function(number1, number2) {
+
+  return number1 - number2;
+
 };
 
-const factorial = function() {
-	
+const sum = function(array) {
+
+  let answer = 0;
+
+  array.forEach(item => {
+    answer += item;
+  });
+
+  return answer;
+};
+
+const multiply = function(array) {
+
+  let answer = 1;
+  array.forEach(item => {
+
+    answer *= item;
+  });
+
+  return answer;
+
+};
+
+const power = function(base, power) {
+
+  let answer = 1;
+  for (let i = 0; i < power; i++) {
+
+    answer *= base;
+  }
+
+  return answer;
+};
+
+const factorial = function(number) {
+
+  let fact = 1;
+  for (let i = 1; i < number + 1; i++) {
+    fact *= i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
