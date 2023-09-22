187756fd604fbdfdde7c723c908c1c03689808ce
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2023-09-22 21:58:13 +0530

    solve

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+  let a = 1;
+  let b = 1;
+
+  number = Number(number);
+  if (number == 1 || number == 2) {
+    return 1;
+  };
+
+  if (number < 0) {
+    return "OOPS";
+  }
+  let sum;
+  for (let i = 0; i < number - 2; i++) {
+
+    sum = a + b;
+    a = b;
+    b = sum;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
